f0186fdf94f59a239c968e80cf1f2910f2f9b44b
bonniss <bonniss@users.noreply.github.com>
2026-03-02 04:35:41 +0000

    deploy: efc560de75ff54dc26b654f82d756ad3315031d8

---
+++ assets/CheckboxField-TSg8vHJF.js
@@ -0,0 +1,1 @@
+import{j as l}from"./index-C5IiHfAi.js";import{a as n}from"./setup-D689D9Ko.js";const x=r=>{const{fieldProps:{id:s,value:i,onChange:e,label:o,disabled:t,readOnly:a,visible:c}}=n();return c?l.jsxs("label",{id:s,children:[l.jsx("input",{type:"checkbox",checked:!!i,onChange:d=>e==null?void 0:e(d.target.checked),disabled:t,readOnly:a,...r}),o]}):null};export{x as C};
