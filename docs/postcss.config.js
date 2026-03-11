3928998631d53fcba919b6b93d428faa7322d600
trung.phamduy User <duytrung.tcu@gmail.com>
2026-03-11 16:35:12 +0700

    fix(docs): use ESM syntax in postcss config

--- docs/postcss.config.js
+++ docs/postcss.config.js
@@ -1,4 +1,4 @@
 // docs/postcss.config.js
 // Empty config to prevent PostCSS from walking up to the root postcss.config.js.
 // The root config pulls in tailwindcss which is not installed in this package.
-module.exports = {};
+export default {};
